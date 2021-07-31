23643bd32d9513b3a5bc73d006ff94c6b7d5257a
minghacker <maming1990@gmail.com>
2021-07-31 16:26:49 -0600

    update test case

--- __test__/QA.test.js
+++ __test__/QA.test.js
@@ -2,15 +2,13 @@ const express = require('../server');
 const supertest = require('supertest');
 const request = supertest(express.app);
 
-
-
 describe('test', () => {
 
   test('responds to a get request with the correct content type ', (done) => {
-    request.get('/qa/questions?product_id=1&page=1')
+    request.get('/qa/questions?product_id=10&page=1')
       .expect('Content-Type', /json/)
       .end((err, res) => {
-        expect(JSON.parse(res.text).results.length).toBe(5);
+        expect(JSON.parse(res.text).results.length).toBeLessThan(5);
         return done();
       })
   });
